41680bb31e7ed303228936dc6c88bacac59ee4e2
Ricardo Mariz <shaaad@gmail.com>
2021-04-28 11:03:53 -0300

    created header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -15,4 +15,19 @@ export const Content = styled.div`
   h1 {
     color: var(--red);
   }
+
+  button {
+    padding: 8px;
+    border-radius: 0.3rem;
+    border: 0;
+    background: var(--blue-light);
+    font-size: 1rem;
+    color: var(--text-body);
+
+    transition: filter 0.2;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
 `;
